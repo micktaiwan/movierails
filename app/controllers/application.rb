40eb7932773b23bc0b8db071ef5a6f891430b79b
faivrem <faivrem@bcd9bb2c-b84f-0410-b794-3bb81658c4ae>
2008-06-17 19:49:22 +0000

    removing login engine

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -1,10 +1,20 @@
 # Filters added to this controller apply to all controllers in the application.
 # Likewise, all the methods added will be available for all controllers.
 
+require 'login_engine'
+  
+ 
 class ApplicationController < ActionController::Base
+  include LoginEngine
   helper :all # include all helpers, all the time
   layout 'general'
 
+  #helper :user
+  model :user
+    
+  before_filter :login_required
+  
+    
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery # :secret => '74f677e56719caa244301e7a8cd6a862'
@@ -13,5 +23,8 @@ class ApplicationController < ActionController::Base
   # Uncomment this to filter the contents of submitted sensitive data parameters
   # from your application log (in this case, all fields with names like "password"). 
   # filter_parameter_logging :password
+  
+  
+  
 end
 
--- config/environment.rb
+++ config/environment.rb
@@ -64,4 +64,12 @@ Rails::Initializer.run do |config|
 
   # Activate observers that should always be running
   # config.active_record.observers = :cacher, :garbage_collector
+ 
+  
 end
+  module LoginEngine
+    config :salt, "superhypersecretphrasethatonlyyouknowsitwow"
+  end
+
+  Engines.start :login
+
