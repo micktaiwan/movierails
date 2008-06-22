68a7a2cc9420886c3c368398c79565d17390b919
faivrem <faivrem@bcd9bb2c-b84f-0410-b794-3bb81658c4ae>
2008-06-22 13:55:26 +0000

    more work

--- trunk/db/migrate/20080615180709_init.rb
+++ trunk/db/migrate/20080615180709_init.rb
@@ -6,18 +6,23 @@ class Init < ActiveRecord::Migration
       table.column :email, :string, :null => false
       table.column :password, :string, :limit => 40, :null => false
       table.column :lost_key, :string
+      table.column :last_login, :datetime
     end
       
     create_table :movies do |table|
       table.column :name, :string, :null => false
-      table.column :imdb, :string
       table.column :year, :string
       table.column :director, :string
     end
       
     create_table :movies_users do |table|
-      table.column :movie_id, :string
-      table.column :user_id, :string
+      table.column :movie_id, :integer
+      table.column :user_id, :integer
+    end
+      
+    create_table :urls do |table|
+      table.column :movie_id, :integer
+      table.column :url, :string
     end
       
   end
@@ -26,6 +31,7 @@ class Init < ActiveRecord::Migration
     drop_table :users
     drop_table :movies
     drop_table :movies_users
+    drop_table :urls
   end
 end
 
