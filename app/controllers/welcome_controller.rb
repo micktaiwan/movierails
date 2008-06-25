bf54e5f0e989bae14cc6dc7565f37d7f2dfd9af3
faivrem <faivrem@bcd9bb2c-b84f-0410-b794-3bb81658c4ae>
2008-06-25 22:32:56 +0000

    comments edition

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,7 +3,7 @@ class WelcomeController < ApplicationController
 
 	def index
 		@my_movies = session['user'].movies
-		@last = Movie.find(:all, :limit=>5, :order=>'created_at desc')
+		@last = Movie.find(:all, :limit=>15, :order=>'created_at desc')
 	end
 	
 end
