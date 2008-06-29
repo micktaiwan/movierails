cc77c1f0dd61c055021e2aac7327a809c24f6ad2
faivrem <faivrem@bcd9bb2c-b84f-0410-b794-3bb81658c4ae>
2008-06-29 11:35:55 +0000

    best movies stats

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,19 @@
 class Movie < ActiveRecord::Base
   has_many :opinions
-	has_many :users, :through=>:opinions 
+	has_many :users, :through=>:opinions
+	
+	def rating
+    nb = 0
+    sum = 0.0
+    Opinion.find(:all, :conditions=>["movie_id = ?",self.id]).each { |o|
+      if o.rating and o.rating > 0
+        nb += 1
+        sum += o.rating
+      end  
+      }
+    return sum / nb if nb > 0
+    return 0
+	end
+	 
 end
 
