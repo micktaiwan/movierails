5d788123dc2e7e2b2296cd0653bc09c9a03283fa
faivrem <faivrem@bcd9bb2c-b84f-0410-b794-3bb81658c4ae>
2008-06-30 20:21:39 +0000

    some ajax feedback

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,11 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+
+function loading(id, bol) {
+  if(bol)
+    $(id).addClassName('loading');
+  else
+    $(id).removeClassName('loading');
+  }
+
